cc7eb9897a07c387d02adf21eeaed843cd52872c
Avery King <avery.n.king@wmich.edu>
2022-03-25 05:59:04 -0400

    url fix, cors added

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const router = require('./routes')
-
+const cors = require('cors');
 const app = express()
+app.use(cors());
 const PORT = process.env.PORT || 5000
 
 app.use('/', router)
